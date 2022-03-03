4329ba6d7d2de9b15d634314e390fe3e2163aebc
Arthur Ulisses Leite <arthurulisses937@gmail.com>
2022-03-03 19:16:21 -0300

    novo site

---
+++ filmes.js
@@ -0,0 +1,99 @@
+const filmes = [
+    { 
+        id: 1,
+        nome: "A filha do rei",
+        categoria: "romance",
+        descricao:
+      "Em busca de imortalidade, o rei Luís XIV rouba a força vital de uma sereia, mas tudo se complica quando sua filha ilegítima cria laços c...",
+    url: "https://raw.githubusercontent.com/Kenzie-Academy-Brasil/lista-filmes-kenzie/main/assets/img/image%20(1).png",
+    },
+    {
+        id: 2,
+        nome: "007 Sem tempo para morrer",
+        categoria: "ação",
+        descricao:
+        "Bond deixou o serviço ativo e está desfrutando de uma vida tranquila na Jamaica. Sua paz é interrompida quando o seu velho amigo Felix Le..",
+      url: "https://raw.githubusercontent.com/Kenzie-Academy-Brasil/lista-filmes-kenzie/main/assets/img/image%20(10).png",
+    },
+    {
+        id: 3,
+        nome: "VENOM Tempo de carnificina",
+        categoria: "ação",
+        descricao:
+        "O jornalista Eddie Brock desenvolve força e poder sobre-humanos quando seu corpo se funde com o alienígena Venom. Dominado pela raiva, Venom tenta controlar as novas e perigosas habilidades de Eddie",
+      url: "https://raw.githubusercontent.com/Kenzie-Academy-Brasil/lista-filmes-kenzie/main/assets/img/image%20(13).png",
+    },
+    {
+        id: 4,
+        nome: "Alerta Vermelho",
+        categoria: "comedia",
+        descricao:
+        "No mundo do crime internacional, a INTERPOL lança um Alerta Vermelho, o que inicia uma caçada para capturar a mais notória ladra de artes do globo",
+      url: "https://raw.githubusercontent.com/Kenzie-Academy-Brasil/lista-filmes-kenzie/main/assets/img/image%20(15).png",
+    },
+    {
+        id: 5,
+        nome: "SHANG-CHI",
+        categoria: "ação",
+        "Shang-Chi é o filho do líder de uma organização criminosa poderosa. O rapaz foi criado desde criança para ser um guerreiro, mas decidiu abandonar esse caminho e fugiu para viver uma vida pacífica...",
+        url: "https://raw.githubusercontent.com/Kenzie-Academy-Brasil/lista-filmes-kenzie/main/assets/img/image%20(16).png",
+    },
+    {
+        id: 6,
+        nome: "Army of Dead",
+        categoria: "ação",
+        descricao:
+      "Nesta história que antecede Army of the Dead, uma mulher misteriosa convida o caixa de banco Dieter para roubar cofres superprotegidos na Europa.",
+    url: "https://raw.githubusercontent.com/Kenzie-Academy-Brasil/lista-filmes-kenzie/main/assets/img/image%20(18).png",
+    },
+    {
+        id: 7,
+        nome: "HALLOWEEN",
+        categoria: "terror",
+        descricao:
+        "Minutos depois de Laurie Strode, sua filha Karen e sua neta Allyson deixarem o assassino mascarado Michael Myers enjaulado e queimando no porão de Laurie",
+      url: "https://raw.githubusercontent.com/Kenzie-Academy-Brasil/lista-filmes-kenzie/main/assets/img/image%20(21).png",
+    },
+    {
+        id: 8,
+        nome: "PÂNICO",
+        categoria: "terror",
+        descricao:
+        "Vinte e cinco anos após uma série de crimes brutais chocar a tranquila Woodsboro, um novo assassino se apropria da máscara de Ghostface e...",
+      url: "https://raw.githubusercontent.com/Kenzie-Academy-Brasil/lista-filmes-kenzie/main/assets/img/image%20(22).png",
+    },
+    {
+        id: 9,
+        nome: "Ghostbusters",
+        categoria: "comedia",
+        descricao:
+        "Uma mãe solteira resolve se mudar para uma pequena cidade do interior com seus filhos, e acaba descobrindo uma conexão com os Caça-Fantas...",
+      url: "https://raw.githubusercontent.com/Kenzie-Academy-Brasil/lista-filmes-kenzie/main/assets/img/image%20(3).png",
+    },
+    {
+        id: 10,
+        nome: "Não olhe para cima",
+        categoria: "drama",
+        categoria: "Comédia",
+        descricao:
+          "Dois astrônomos descobrem um cometa mortal vindo em direção à Terra e partem em um tour midiático para alertar a humanidade. Só que ninguém parece dar muita bola.",
+        url: "https://raw.githubusercontent.com/Kenzie-Academy-Brasil/lista-filmes-kenzie/main/assets/img/image%20(5).png",
+    },
+    {
+        id: 11,
+        nome: "MATRIX",
+        categoria: "ação",
+        descricao:
+        "quarta parcela da franquia Matrix. Trama ainda desconhecida....",
+      url: "https://raw.githubusercontent.com/Kenzie-Academy-Brasil/lista-filmes-kenzie/main/assets/img/image%20(6).png",
+    },
+    {
+        id: 12,
+        nome: "Resident Evil",
+        categoria: "terror",
+        descricao:
+        "O que já foi um dia a casa da gigante farmacêutica em expansão Umbrella Corporation, Raccoon City, é agora uma cidade agonizante do meio-oeste.",
+      url: "https://raw.githubusercontent.com/Kenzie-Academy-Brasil/lista-filmes-kenzie/main/assets/img/image%20(7).png",
+    },
+    
+];
